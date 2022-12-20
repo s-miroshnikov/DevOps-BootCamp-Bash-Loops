867d28c83e28f3f20872dd0378247c9370b36094
s.miroshnikov <97443660+s-miroshnikov@users.noreply.github.com>
2022-12-20 11:44:54 +0400

    Update loops1.sh

--- loops1.sh
+++ loops1.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-# Put your code here
+str=$1
+# for while loop we need lenght of the string
+strl=${#str}
+i=$((strl-1))
+# in this while loop we extracting characters one by one, starting from last character and decreasing index
+while [ $i -ge 0 ]
+do
+    revstr=$revstr${str:$i:1}
+    i=$((i-1))
+done
+# with 'tr' we will change uppercase to lowercase and back in the same time
+tr '[:upper:][:lower:]' '[:lower:][:upper:]' <<< "$revstr"
